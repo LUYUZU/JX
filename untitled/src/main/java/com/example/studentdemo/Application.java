6ec8dd0f88d5a00a6a2c419a5aa98e45f241d267
鹿月竹 <15877391+deer-moon-bamboo@user.noreply.gitee.com>
2026-04-18 19:23:34 +0800

    更新端口

--- untitled/src/main/java/com/example/studentdemo/Application.java
+++ untitled/src/main/java/com/example/studentdemo/Application.java
@@ -8,6 +8,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("学生信息管理系统启动成功！");
-        System.out.println("访问地址: http://localhost:8080");
+        System.out.println("访问地址: http://localhost:9000");
     }
 }
